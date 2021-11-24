39b71f41ef8dddf4a20916395a8bb0f84b093f94
joker153 <77005994+joker153@users.noreply.github.com>
2021-11-24 19:00:18 +0530

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -4,22 +4,22 @@ return `DONASI BISA VIA BANK / E-MONEY
 *🏧 BANK*
 
 1. BRI 
-	ㅁ 8881 0887 4350 47326
+	ㅁ 
 2. BCA 
-	ㅁ 3901 0887 4350 47326
+	ㅁ 
 3. BNI 
-	ㅁ 881 0887 4350 47326
+	ㅁ 
 4. PERMATA
-	ㅁ 852 8887 4350 47326
+	ㅁ 
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ 
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 
 	
 Semuanya atas nama RI****DI
 `

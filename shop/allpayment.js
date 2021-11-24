3fa019bfa8a7548a23e01217a4cf890fd9a1fb41
joker153 <77005994+joker153@users.noreply.github.com>
2021-11-24 18:52:11 +0530

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,25 +1,25 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
+return `PAYMENT BY AMAL
 
 *🏧 BANK*
 
 1. BRI 
-	ㅁ 8881 0887 4350 47326
+	ㅁ 
 2. BCA 
-	ㅁ 3901 0887 4350 47326
+	ㅁ 
 3. BNI 
-	ㅁ 881 0887 4350 47326
+	ㅁ 
 4. PERMATA
-	ㅁ 852 8887 4350 47326
+	ㅁ 
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ 
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 
 	
 Semuanya atas nama RI****DI
 
